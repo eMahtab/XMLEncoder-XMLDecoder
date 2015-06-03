f37cfb003df843c665f7fb244ad3070fab032227
eMahtab <alammahtab08@gmail.com>
2015-06-03 19:18:25 +0530

    XMLEncoder XMLDecoder

---
+++ src/co/edureka/DVD.java
@@ -0,0 +1,28 @@
+package co.edureka;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DVD {
+	
+	private List<Movie> movies=new ArrayList<>();
+	
+	public DVD(){}
+
+	public List<Movie> getMovies() {
+		return movies;
+	}
+
+	public void setMovies(List<Movie> movies) {
+		this.movies = movies;
+	}
+	
+	public String toString(){
+		String movies="";
+		for(Movie movie:getMovies()){
+			movies += movie.getName()+", ";
+		}
+		return movies; 
+	}
+	
+}
